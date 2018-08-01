c683b8fdc4644346783912f20fff39e98e9ee1cf
Ms-Alex <ms.hernandeza1@gmail.com>
2018-08-01 17:59:40 -0400

    beehives are falling

--- src/beehive.js
+++ src/beehive.js
@@ -1,40 +1,53 @@
+// document.addEventListener('DOMContentLoaded',function(event) {
 
 
-let counter = 0
+  let counter = 0
 
 
-function createBeehive(){
-  let randomNumber = Math.floor(Math.random() * 700) + 100;
-  let beehiveId = counter
-  const gameContainer = document.getElementById('container')
+  function createBeehive(){
+    counter++
+    let randomNumber = Math.floor(Math.random() * 700) + 100;
+    let beehiveId = counter
+    const gameContainer = document.getElementById('container')
+    // const body = document.getElementsByTagName('body')[0]
 
 
-  const beehiveHTML = `<div class="beehive" style="left: ${randomNumber}px; top: 175px; position: absolute"> <img src="https://png.icons8.com/cotton/2x/hornet-hive.png" width="80px" height="80px"> </div>`
+    const beehiveHTML = `<div id="beehive_${counter}" style="left: ${randomNumber}px; top: 175px; position: absolute"> <img src="https://png.icons8.com/cotton/2x/hornet-hive.png" width="80px" height="80px"> </div>`
+    gameContainer.innerHTML += beehiveHTML
+    // body.innerHTML += beehiveHTML
 
-  gameContainer.innerHTML += beehiveHTML
-
-}
+  }
 
 
-// setInterval(myMove, 1500);
+  // setInterval(myMove, 2000);
 
-function myMove() {
-  createBeehive()
+  function myMove() {
+    createBeehive()
+    let myCounter = counter
+    let originalElement = document.getElementById(`beehive_${myCounter}`);
 
-  let elem = document.getElementById(`beehive_${counter}`);
+    // x position will equal this
+    let pos = 160;
+    let id = setInterval(frame, 5);
+    function frame() {
+      let elem = document.getElementById(`beehive_${myCounter}`);
+      //console.log(elem===originalElement)
+      if (pos == 680) {
+        clearInterval(id);
+        elem.remove()
+      } else {
+        pos++;
+        elem.style.top = pos + 'px';
 
-  // x position will equal dis
-  let pos = 160;
-  let id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 680) {
-      clearInterval(id);
-      elem.remove()
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-
+      }
     }
   }
 
-}
+
+  // function touchesBasket?(elem){
+  //   if (elem.style.left === basket.style.left && ) {
+  //
+  //   }
+  // }
+
+// })
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   const usernameInput = document.getElementById('username-input')
   const loginForm = document.getElementById('login-form')
+  const scoreContainer = document.getElementById('score-container')
 
 
 ////////////////////
@@ -18,21 +19,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     loginUser(postBody).then(data => new User(data))
 
+    loginForm.remove()
+    setInitialLives()
+    setScore()
+    setInterval(myMove, 2000);
+
+
   })
 
-  const basket = document.getElementById('medkit1')
+  const basket = document.getElementById('basket')
   document.addEventListener('keydown', (event) => {
 
     // if keypress left
     if (event.keyCode == '37') {
-      if ( basket.style.left !== "90px" ) {
+      if ( basket.style.left !== "100px" ) {
         basket.style.left = parseInt(basket.style.left) - 5 + "px"
       }
 
     }
     // if keypress right
     if (event.keyCode == '39') {
-      if ( basket.style.left !== "845px" ) {
+      if ( basket.style.left !== "800px" ) {
         basket.style.left = parseInt(basket.style.left) + 5 + "px"
       }
 
@@ -41,12 +48,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   /////////////////
 
-  // myMove()
 
+  // setInterval(myMove, 2000);
 
+  function setInitialLives(){
+    const heartsContainer = document.getElementById('hearts-container')
 
+    let first_heart = `
+      <div id="heart_1" style="left: 115px; top: 175px; position: absolute"> <img src="https://vignette.wikia.nocookie.net/unturned-bunker/images/0/0f/Medkit.png/revision/latest?cb=20160523170332" width="75px" height="50px"> </div>
+    `
 
-  // myMove()
+    let sec_heart = `
+      <div id="heart_2" style="left: 195px; top: 175px; position: absolute"> <img src="https://vignette.wikia.nocookie.net/unturned-bunker/images/0/0f/Medkit.png/revision/latest?cb=20160523170332" width="75px" height="50px"> </div>
+    `
+
+    let third_heart = `
+      <div id="heart_3" style="left: 275px; top: 175px; position: absolute"> <img src="https://vignette.wikia.nocookie.net/unturned-bunker/images/0/0f/Medkit.png/revision/latest?cb=20160523170332" width="75px" height="50px"> </div>
+    `
+
+    heartsContainer.innerHTML = first_heart + sec_heart + third_heart
+  }
+
+  function setScore() {
+    let scoreText = `<h3>Score: 0</h3>`
+    scoreText.innerHTML = scoreText
+
+  }
+
 
 
 });
